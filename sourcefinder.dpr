ada2d8419e6a9c0606137f5f7fe5e703f7bbc8f5
plugwash <plugwash@96073cd6-c582-4674-a924-5ef5d2f3c6a3>
2013-05-03 23:48:31 +0000

    add total counting and error exit functionality to sourcefinder
    git-svn-id: svn+ssh://svn.debian.org/svn/raspbian/raspbian-tools@13 96073cd6-c582-4674-a924-5ef5d2f3c6a3

--- sourcefinder.dpr
+++ sourcefinder.dpr
@@ -1,7 +1,8 @@
 program sourcefinder;
 uses
   readtxt2,sysutils,util;
-
+type
+  packageresults = (prfound,prcopied,prnotfound,prcopyfail,prverifyfail);
 var
   t : treadtxt;
   line : string;
@@ -18,7 +19,14 @@ var
   verifyroot : string;
   dirname : string;
   p : integer;
+  packageresult : packageresults;
+  foundcount, copiedcount, notfoundcount, copyfailcount, verifyfailcount : integer;
 begin
+  foundcount := 0;
+  copiedcount := 0;
+  notfoundcount := 0;
+  copyfailcount := 0;
+  verifyfailcount := 0;
   t := treadtxt.createf(paramstr(1));
   docopy := (paramstr(2) = '--copy');
   repeat
@@ -43,25 +51,54 @@ begin
           executeresult := executeprocess('/usr/bin/dcmd','cp '+root+filepath+' '+targetroot+dirname);
           if executeresult = 0 then begin
             writeln(filepath+' successfully copied to target directory');
+            packageresult := prcopied;
             verifyroot := targetroot;
           end else begin
+            packageresult := prcopyfail;
             writeln(filepath+' !!!failure!!! whilecopying');
             //no point in doing a verify if the copy failed
             verifyroot := '';
           end;          
         end else begin
+          packageresult := prfound;
           verifyroot := root;
           writeln(root+filepath);
         end;
         if verifyroot <> '' then begin
           flush(output); //flush standard output to preserve sane ordering
           executeresult := executeprocess('/usr/bin/dscverify','-u '+verifyroot+filepath);
-          if executeresult <> 0 then writeln(filepath+' !!!failure!!!, source package failed to verify');
+          if executeresult <> 0 then begin
+            writeln(filepath+' !!!failure!!!, source package failed to verify');
+            packageresult := prverifyfail;
+          end;
         end;
         found := true;
         break;
       end;
     end;
-    if not found then writeln(filepath+' !!!failure!!!, could not find source package');
+    if not found then begin 
+      writeln(filepath+' !!!failure!!!, could not find source package');
+      packageresult := prnotfound;
+    end;
+    case packageresult of
+      prfound : foundcount := foundcount + 1;
+      prcopied : copiedcount := copiedcount +1;
+      prnotfound : notfoundcount := notfoundcount +1;
+      prcopyfail : copyfailcount := copyfailcount +1;
+      prverifyfail : verifyfailcount := verifyfailcount +1;
+    end;
   until t.eof;
+  if docopy then begin
+    writeln(foundcount,' found already in the right place');
+    writeln(copiedcount,' found and copied');
+  end else begin
+    writeln(foundcount,' found');
+  end;
+  writeln(notfoundcount,' not found');
+  writeln(copyfailcount,' failed to copy');
+  writeln(verifyfailcount,' failed to verify');
+  if (notfoundcount > 0) or (copyfailcount > 0) or (verifyfailcount > 0) then begin
+    writeln('exiting with error');
+    halt(1);
+  end;
 end.
