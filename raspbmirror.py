3134d5bd32225b680b4804480fe562008da770bf
Peter Michael Green <plugwash@raspbian.org>
2020-06-28 02:37:41 +0100

    Report rejected filename in ensuresafepath.

--- raspbmirror.py
+++ raspbmirror.py
@@ -96,7 +96,7 @@ def ensuresafepath(path):
 		sys.exit(1)
 	for component in pathsplit:
 		if not pfnallowed.fullmatch(component):
-			print("file name contains unexpected characters")
+			print("file name "+ascii(filename)+" contains unexpected characters")
 			sys.exit(1)
 		elif component[0] == '.':
 			print("filenames starting with a dot are not allowed")
